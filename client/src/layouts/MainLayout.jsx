218d40473d159512e48d3bc08c1abe9193ef88a8
Rahul <rahul@Rahuls-MacBook-Air.local>
2026-07-20 16:45:16 +0530

    Revert "Integrate Lenis inertia smooth scroll engine for luxury Apple web experience"
    This reverts commit 0539e10339560cafaa45211c43078f671a9f7b2b.

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -8,8 +8,6 @@ import Navbar from '../components/Navbar';
 import CartDrawer from '../components/CartDrawer';
 import Footer from '../components/Footer';
 
-import Lenis from 'lenis';
-
 export default function MainLayout() {
   const { pathname } = useLocation();
   const dispatch = useDispatch();
@@ -22,36 +20,12 @@ export default function MainLayout() {
     }
   }, [dispatch, isAuthenticated]);
 
-  // Lenis Smooth Scroll Engine
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothWheel: true,
-      wheelMultiplier: 1,
-      touchMultiplier: 1.5
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
     });
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    window.lenis = lenis;
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (window.lenis) {
-      window.lenis.scrollTo(0, { immediate: true });
-    } else {
-      window.scrollTo(0, 0);
-    }
   }, [pathname]);
 
   return (
